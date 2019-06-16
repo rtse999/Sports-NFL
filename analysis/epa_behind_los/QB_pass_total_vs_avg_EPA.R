84242a017bcd11f5e03c4a445ebed5461d93b5af
rtse999 <rtse999@gmail.com>
2019-06-16 14:48:15 +1000

    Added section headings to code. Added column: pass_to_los_pct

--- analysis/epa_behind_los/QB_pass_total_vs_avg_EPA.R
+++ analysis/epa_behind_los/QB_pass_total_vs_avg_EPA.R
@@ -1,5 +1,4 @@
-# ------------------------------------------------------------------------
-# Average vs Total EPA for passing plays 2018
+# Average vs Total EPA for passing plays 2018 -----------------------------
 #
 # Link: https://twitter.com/JayCromwell12/status/1139294494036242432
 # Link: https://twitter.com/rtse999/status/1140074416094642178
@@ -7,34 +6,23 @@
 # Location: /Users/raymondtse/Dropbox/Analysis/Sports/NFL/analysis/epa_nehind_los/QB_pass_total_vs_avg_EPA.r
 # First created: 16:24 - Saturday 15 June 2019
 # Last modified: 16:24 - Saturday 15 June 2019
-# ------------------------------------------------------------------------
 
-# ------------------------------------------------------------------------
-# System time
-# ------------------------------------------------------------------------
+# System time -------------------------------------------------------------
 format(Sys.time(), "%a %b %d %H:%M:%S %Y")
 
-# ------------------------------------------------------------------------
-# Install Packages
-# ------------------------------------------------------------------------
+# Install Packages --------------------------------------------------------
 library(conflicted)
 library(ggrepel)
 library(skimr)
 library(tidyverse)
 
-# ------------------------------------------------------------------------
-# Load functions
-# ------------------------------------------------------------------------
+# Load functions ----------------------------------------------------------
 source(file = "/Users/raymondtse/Dropbox/Analysis/Sports/NFL/src/functions_pbp/fn_read_pbp_data.r")
 
-# ------------------------------------------------------------------------
-# Read data
-# ------------------------------------------------------------------------
+# Read data ---------------------------------------------------------------
 reg_pbp_2018 <- read_pbp_data("/Users/raymondtse/Dropbox/Analysis/github packages/nflscrapR-data/play_by_play_data/regular_season/reg_pbp_2018.csv")
 
-# ------------------------------------------------------------------------
-# Create data set
-# ------------------------------------------------------------------------
+# Create data set ---------------------------------------------------------
 # Refactor to function
 non_penalty_plays <- 
   reg_pbp_2018 %>% 
@@ -43,17 +31,19 @@ non_penalty_plays <-
 
 levels(non_penalty_plays$play_type)
 
-# Select only passes
+# Select only passes ------------------------------------------------------
 passes <-
   non_penalty_plays %>%
   dplyr::filter(play_type == "pass")
   
-# Calculate total and average EPA by QB
+# Calculate total and average EPA by QB -----------------------------------
 qb_epa <- 
   passes %>% 
   group_by(passer_player_name) %>% 
   summarise(
     n_play = n(),
+    completions = sum(complete_pass),
+    completion_pct = completions / n_play,
     total_epa = sum(epa, na.rm = TRUE),
     avg_epa = mean(epa, na.rm = TRUE)
   ) %>% 
@@ -61,23 +51,21 @@ qb_epa <-
   arrange(-total_epa) %>% 
   dplyr::filter(n_play > 50) 
 
-# Calculate total and average EPA by QB for completed passes only
+# Calculate total and average EPA by QB for completed passes only ---------
 qb_completions_epa <- 
   passes %>% 
   dplyr::filter(complete_pass == 1) %>% 
   group_by(passer_player_name) %>% 
   summarise(
     n_play = n(),
+    completions = sum(complete_pass),
+    completion_pct = completions / n_play,
     total_epa = sum(epa, na.rm = TRUE),
     avg_epa = mean(epa, na.rm = TRUE)
   ) %>% 
   ungroup() %>% 
   arrange(-total_epa) %>% 
   dplyr::filter(n_play > 50) 
-
-qb_completions_epa %>% 
-  ggplot() +
-  geom_point(aes(x = avg_epa, y = total_epa))
 
 # Calculate total and average EPA by QB for passes up to line of scrimmage
 qb_los_epa <- 
@@ -95,6 +83,18 @@ qb_los_epa <-
   arrange(-total_epa) %>% 
   dplyr::filter(n_play > 50)
 
+# Behind LOS plays as proportion of total ---------------------------------
+qb_pass_comparison <- 
+  left_join(qb_los_epa, qb_epa, by = "passer_player_name") %>% 
+  mutate(
+    pass_to_los_pct = n_play.x / n_play.y
+  ) %>% 
+  select(passer_player_name, pass_to_los_pct)
+
+qb_los_epa <- left_join(qb_los_epa, qb_pass_comparison, 
+                        by = "passer_player_name")
+
+# QB LOS chart ------------------------------------------------------------
 qb_los_epa_labels <-
   qb_los_epa %>% 
   slice(1:5, (nrow(qb_los_epa)-3):nrow(qb_los_epa))
@@ -114,6 +114,7 @@ qb_los_epa %>%
     y = "Total EPA"
   )
 
+# Overall LOS stats -------------------------------------------------------
 # Total attempts behind LOS 
 sum(qb_los_epa$n_play)
 
@@ -122,7 +123,6 @@ sum(qb_los_epa$completions)
 
 # Completion rate behind LOS 
 sum(qb_los_epa$completions) / sum(qb_los_epa$n_play)
-
 
 # Calculate total and average EPA by Andrew Luck --------------------------
 andrew_luck_los_epa <- 
@@ -155,8 +155,6 @@ mean(andrew_luck_los_epa$epa, na.rm = TRUE)
 # Why does play_id == 4247 have epa == NA when it shouldn't ?
 err <- dplyr::filter(andrew_luck_los_epa, is.na(epa))
 
-
-
 # Calculate total and average EPA by Patrick Mahomes --------------------------
 patrick_mahomes_los_epa <- 
   passes %>% 
@@ -181,7 +179,6 @@ patrick_mahomes_los_epa %>%
     x = "Yards Gained",
     y = "Attempts"
   )
-
 
 # Calculate total and average EPA by QB for completed passes up to --------
 qb_completions_los_epa <- 
