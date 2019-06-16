54885eac175d72d357e5673ff6bd2a806b6a5ea2
rtse999 <rtse999@gmail.com>
2019-06-16 14:11:05 +1000

    Renamed directory. Added bottom 5 labels. Andrew Luck & Patrick Mahomes charts

--- analysis/epa_behind_los/QB_pass_total_vs_avg_EPA.R
+++ analysis/epa_behind_los/QB_pass_total_vs_avg_EPA.R
@@ -2,8 +2,9 @@
 # Average vs Total EPA for passing plays 2018
 #
 # Link: https://twitter.com/JayCromwell12/status/1139294494036242432
+# Link: https://twitter.com/rtse999/status/1140074416094642178
 #
-# Location: /Users/raymondtse/Dropbox/Analysis/Sports/NFL/analysis/QBs/QB_pass_total_vs_avg_EPA.r
+# Location: /Users/raymondtse/Dropbox/Analysis/Sports/NFL/analysis/epa_nehind_los/QB_pass_total_vs_avg_EPA.r
 # First created: 16:24 - Saturday 15 June 2019
 # Last modified: 16:24 - Saturday 15 June 2019
 # ------------------------------------------------------------------------
@@ -17,6 +18,7 @@ format(Sys.time(), "%a %b %d %H:%M:%S %Y")
 # Install Packages
 # ------------------------------------------------------------------------
 library(conflicted)
+library(ggrepel)
 library(skimr)
 library(tidyverse)
 
@@ -84,6 +86,8 @@ qb_los_epa <-
   group_by(passer_player_name) %>% 
   summarise(
     n_play = n(),
+    completions = sum(complete_pass),
+    completion_pct = completions / n_play,
     total_epa = sum(epa, na.rm = TRUE),
     avg_epa = mean(epa, na.rm = TRUE)
   ) %>% 
@@ -93,14 +97,14 @@ qb_los_epa <-
 
 qb_los_epa_labels <-
   qb_los_epa %>% 
-  slice(1:5)
+  slice(1:5, (nrow(qb_los_epa)-3):nrow(qb_los_epa))
 
 qb_los_epa %>% 
   ggplot() +
   geom_vline(xintercept = 0, colour = "white", size = 3) +
   geom_hline(yintercept = 0, colour = "white", size = 3) +
   geom_point(aes(x = avg_epa, y = total_epa)) +
-  geom_text(aes(label = passer_player_name, x = avg_epa, 
+  geom_text_repel(aes(label = passer_player_name, x = avg_epa, 
                                 y = total_epa),
             data = qb_los_epa_labels,
             nudge_y = 3, alpha = 0.5) +
@@ -110,8 +114,76 @@ qb_los_epa %>%
     y = "Total EPA"
   )
 
+# Total attempts behind LOS 
+sum(qb_los_epa$n_play)
 
-# Calculate total and average EPA by QB for completed passes up to line of scrimmage only
+# Total completions behind LOS 
+sum(qb_los_epa$completions)
+
+# Completion rate behind LOS 
+sum(qb_los_epa$completions) / sum(qb_los_epa$n_play)
+
+
+# Calculate total and average EPA by Andrew Luck --------------------------
+andrew_luck_los_epa <- 
+  passes %>% 
+  dplyr::filter(air_yards <= 0, passer_player_name == "A.Luck")
+
+andrew_luck_los_epa %>% 
+  ggplot() +
+  geom_histogram(aes(x = epa), bins = 10) +
+  geom_vline(xintercept = 0, colour = "white", size = 1) +
+  labs(
+    title = paste("Attempted passes at or behind LOS by A.Luck - 2018"),
+    x = "EPA",
+    y = "Attempts"
+  )
+
+andrew_luck_los_epa %>% 
+  ggplot() +
+  geom_histogram(aes(x = yards_gained), bins = 10) +
+  geom_vline(xintercept = 0, colour = "white", size = 1) +
+  labs(
+    title = paste("Yards gained on passes at or behind LOS by A.Luck - 2018"),
+    x = "Yards Gained",
+    y = "Attempts"
+  )
+
+# Average EPA for Andrew Luck on passes behind the LOS is ...
+mean(andrew_luck_los_epa$epa, na.rm = TRUE)
+
+# Why does play_id == 4247 have epa == NA when it shouldn't ?
+err <- dplyr::filter(andrew_luck_los_epa, is.na(epa))
+
+
+
+# Calculate total and average EPA by Patrick Mahomes --------------------------
+patrick_mahomes_los_epa <- 
+  passes %>% 
+  dplyr::filter(air_yards <= 0, passer_player_name == "P.Mahomes")
+
+patrick_mahomes_los_epa %>% 
+  ggplot() +
+  geom_histogram(aes(x = epa), bins = 10) +
+  geom_vline(xintercept = 0, colour = "white", size = 1) +
+  labs(
+    title = paste("Attempted passes at or behind LOS by P.Mahomes - 2018"),
+    x = "EPA",
+    y = "Attempts"
+  )
+
+patrick_mahomes_los_epa %>% 
+  ggplot() +
+  geom_histogram(aes(x = yards_gained), bins = 10) +
+  geom_vline(xintercept = 0, colour = "white", size = 1) +
+  labs(
+    title = paste("Yards gained on passes at or behind LOS by P.Mahomes - 2018"),
+    x = "Yards Gained",
+    y = "Attempts"
+  )
+
+
+# Calculate total and average EPA by QB for completed passes up to --------
 qb_completions_los_epa <- 
   passes %>% 
   dplyr::filter(complete_pass == 1) %>% 
